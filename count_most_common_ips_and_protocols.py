1376977b6e734ae02bec8e6d43ba7ed864d7497b
Elisa Stresemann <56543931+ElisaStresemann@users.noreply.github.com>
2024-11-27 21:57:51 +0100

    Update count_most_common_ips_and_protocols.py

--- count_most_common_ips_and_protocols.py
+++ count_most_common_ips_and_protocols.py
@@ -1,7 +1,7 @@
 import pyshark
 from collections import Counter
 
-# Lade und öffne PCAP-Datei für Analyse
+# Lade und oeffne PCAP-Datei für Analyse
 cap = pyshark.FileCapture('sectry.pcap')
 
 # Test: Drucke das erste Paket
@@ -9,26 +9,36 @@ cap = pyshark.FileCapture('sectry.pcap')
 #    print(packet)  
 #    break  # Nur das erste Paket drucken, dann Abbruch
 
-# Zähler für IP-Adressen und Protokolle
-ip_counter = Counter()
+# Zaehler für IP-Adressen und Protokolle
+# Counter-Objekt 
+ipv4_counter = Counter()
+ipv6_counter = Counter()
 protocol_counter = Counter()
 
 # Durchlaufe alle Pakete 
 for packet in cap:
-    # Zähle IPs
+    # Zaehle IPs
     if 'IP' in packet:
-        ip_counter[packet.ip.src]+=1
-        ip_counter[packet.ip.dst]+=1
+        ipv4_counter[packet.ip.src]+=1
+        ipv4_counter[packet.ip.dst]+=1
+    #'IPv6' zaehlt zusaetzlich IPv6-Pakete
+    elif 'IPv6' in packet:
+        ipv6_counter[packet.ipv6.src]+=1
+        ipv6_counter[packet.ipv6.dst]+=1
 
-    # Zähle Protokolle
+    # Zaehle Protokolle
     if 'IP' in packet:
         protocol_counter[packet.transport_layer]+=1
 
 
-# Ausgabe häufigste IP-Adresse
-most_common_ip = ip_counter.most_common(1)
-print(f"Häufigste IP-Adresse: {most_common_ip[0][0]} mit {most_common_ip[0][1]} Paketen")
+# Ausgabe haeufigste IP-Adresse - most_common() Methode aus Counter - ohne Parameter Auflistung der Haeufigkeiten in absteigender Reihenfolge
+most_common_ipv4 = ipv4_counter.most_common(2)
+print(most_common_ipv4)
+print(f"Häufigste IPv4-Adresse: {most_common_ipv4[0][0]} mit {most_common_ipv4[0][1]} Paketen und {most_common_ipv4[1][0]} mit {most_common_ipv4[1][1]}")
 
-# Ausgabeäufigstes Protokoll
+most_common_ipv6 = ipv6_counter.most_common(1)
+print(f"Häufigste IPv6-Adresse: {most_common_ipv6[0][0]} mit {most_common_ipv6[0][1]} Paketen")
+
+# Ausgabeaeufigstes Protokoll
 most_common_protocol = protocol_counter.most_common(1)
-print(f"Häufigstes Protokoll: {most_common_protocol[0][0]} mit {most_common_protocol[0][1]} Vorkommen")
+print(f"Haeufigstes Protokoll: {most_common_protocol[0][0]} mit {most_common_protocol[0][1]} Vorkommen")
